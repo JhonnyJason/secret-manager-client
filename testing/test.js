fb3eb4f5e02c7f1caa51e29f27ea62a60b2ca9a5
Lenard Frommelt <lenny09918050@googlemail.com>
2020-11-11 10:38:05 +0100

    thingysync push

---
+++ testing/test.js
@@ -0,0 +1,20 @@
+const leModule = require("../output/index.js")
+
+async function run() {
+    const client = await leModule.createClient(null, null, "https://secrets.extensivlyon.coffee")
+    const privateKey = client.secretKeyHex
+    const publicKey = client.publicKeyHex
+
+    const mySecret = "The world is mine!"
+
+    await client.setSecret("mySecret", mySecret)
+    const retrievedSecret = await client.getSecret("mySecret")
+    
+    console.log(retrievedSecret)
+
+}
+
+
+
+
+run()

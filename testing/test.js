3226e787abdeb06ab251302c046a63086e26dd19
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-04 19:55:20 +0200

    thingysync push

--- testing/test.js
+++ testing/test.js
@@ -4,6 +4,7 @@ async function run() {
     const client = await leModule.createClient(null, null, "https://secrets.extensivlyon.coffee")
     const privateKey = client.secretKeyHex
     const publicKey = client.publicKeyHex
+    console.log("successfully created the client!")
 
     const mySecret = "The world is mine!"
 

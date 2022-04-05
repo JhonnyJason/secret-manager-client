e1d2753cd48ae0ad30f2f7e1a0a330d3411ea3ff
Lenard Frommelt <lenny09918050@gmail.com>
2022-04-05 13:00:09 +0200

    thingysync push

--- testing/test.mjs
+++ testing/test.mjs
@@ -1,8 +1,9 @@
-const leModule = require("../output/index.js")
+import * as fac from "../output/index.js"
+
 process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 async function run() {
-    const client = await leModule.createClient(null, null, "https://localhost:6999")
+    const client = await fac.createClient(null, null, "https://localhost:6999")
     const privateKey = client.secretKeyHex
     const publicKey = client.publicKeyHex
     console.log("successfully created the client!")
